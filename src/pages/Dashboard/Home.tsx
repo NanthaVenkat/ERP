43183438d9d15e85d483914eee4eb99b12a147ae
NnthaKumar <vnanthakumar00@gmail.com>
2026-04-09 10:52:05 +0530

    Add DatePicker component and update ecommerce metrics layout

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -3,8 +3,8 @@ import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
-// import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
+// import DemographicCard from "../../components/ecommerce/DemographicCard";
 
 export default function Home() {
   return (
@@ -24,16 +24,16 @@ export default function Home() {
           <MonthlyTarget />
         </div>
 
-        <div className="col-span-12">
-          <StatisticsChart />
-        </div>
-
         {/* <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
         </div> */}
 
         <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
+        </div>
+
+        <div className="col-span-12">
+          <StatisticsChart />
         </div>
       </div>
     </>
